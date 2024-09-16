301078123800fea3ec14f1599a9a0d192cdacaa0
Fabiano Frizzo <ffrizzo@gmail.com>
2024-09-16 07:03:03 -0300

    Update available subnets from two to three

--- infrastructure/vpc/main.tf
+++ infrastructure/vpc/main.tf
@@ -11,14 +11,14 @@ module "vpc" {
   name = "sample-vpc"
   cidr = "10.0.0.0/16"
 
-  azs = ["us-west-1a", "us-west-1c"]
+  azs = ["us-west-1a", "us-west-1b", "us-west-1c"]
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnet_tags = {
     type = "private"
   }
 
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   public_subnet_tags = {
     type = "public"
   }
